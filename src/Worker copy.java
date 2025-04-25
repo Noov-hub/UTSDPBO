033b98a41b70be7a0d54b619a1576ef55a51c5d6
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-04-25 22:16:06 +0700

    deleted:    src/Produk copy.java 	deleted:    src/Worker copy.java

--- src/Worker copy.java
+++
@@ -1,7 +0,0 @@
-/*Saya Ibnu Fadhilah dengan NIM 2300613 mengerjakan UTS dalam mata kuliah Desain dan
-        Pemrograman Berorientasi Objek untuk keberkahanNya maka saya tidak melakukan kecurangan
-        seperti yang telah dispesifikasikan. Aamiin.
-*/
-public interface Worker {
-    void displayInfo();
-}
