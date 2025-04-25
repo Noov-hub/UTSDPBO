02f1e9144c6592f2cef096d172a03f4e825b2a7c
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-04-25 22:14:24 +0700

    new file:   src/Perusahaan.java 	new file:   src/PerusahaanPengiriman.java 	new file:   src/Produk.java 	new file:   src/Worker.java

---
+++ src/Worker.java
@@ -0,0 +1,7 @@
+/*Saya Ibnu Fadhilah dengan NIM 2300613 mengerjakan UTS dalam mata kuliah Desain dan
+        Pemrograman Berorientasi Objek untuk keberkahanNya maka saya tidak melakukan kecurangan
+        seperti yang telah dispesifikasikan. Aamiin.
+*/
+public interface Worker {
+    void displayInfo();
+}
